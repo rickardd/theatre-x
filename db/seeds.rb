d2eba2dde226738d706d44d2887f122f421af6b4
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2016-08-05 11:57:35 +0200

    seed file updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,18 +25,23 @@
 # ShowPrice.create( price: 80, description: "Skolrabatt: minst 30 biljetter.", primary: false, show_id: 2 )
 # ShowPrice.create( price: 80, description: "Föreldrar rabatt: minst 1 barn.", primary: false, show_id: 2 )
 
-# About.create( column_1: "Teater (namn) är en helt nystartad teater och vänder sig till både barn och vuxna.. Pjäserna är specialskrivna för teatern och integrerar oftast publiken på olika sätt. Teaterns mål är att alltid vara lätt tillgänglig och för alla. Det är två inplanerade produktioner under 2017 en barn teater under vinter/våren och en familjeföreställning i form av vandringsteater på sommaren. Spelplatserna varierar beroende på föreställning.",
-#               column_2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id dolorem nulla earum autem, dolor rerum illo velit veniam, quam neque, recusandae fugiat voluptates optio dignissimos odit debitis iste inventore a magni quis, ipsum aliquid. Quibusdam nam modi error quasi blanditiis veniam dolorem pariatur velit sunt. Harum dignissimos, laborum quibusdam. Aliquam!",
-#               column_3: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus odio pariatur repudiandae eos vel, porro nequt excepturi minus vel officiis voluptates minima repellat, eos laudantium sequi nesciunt. Ducimus, veniam, porro!",
-#               title: "Om teatern",
-#               image: "anja.png" )
+About.create( column_1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis voluptates autem labore laboriosam, nam dignissimos officia neque provident vitae eum.",
+              column_2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores numquam recusandae quia totam, quis optio debitis quidem, odio in quam?",
+              column_3: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni delectus vitae sunt, saepe dignissimos ducimus similique adipisci. Cupiditate, optio placeat.",
+              title: "Om teatern",
+              image: "placeholder.png" )
 
 
-# CoOperation.create( column_1: "Teater (namn) är en helt nystartad teater och vänder sig till både barn och vuxna.. Pjäserna är specialskrivna för teatern och integrerar oftast publiken på olika sätt. Teaterns mål är att alltid vara lätt tillgänglig och för alla. Det är två inplanerade produktioner under 2017 en barn teater under vinter/våren och en familjeföreställning i form av vandringsteater på sommaren. Spelplatserna varierar beroende på föreställning.",
-#               column_2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id dolorem nulla earum autem, dolor rerum illo velit veniam, quam neque, recusandae fugiat voluptates optio dignissimos odit debitis iste inventore a magni quis, ipsum aliquid. Quibusdam nam modi error quasi blanditiis veniam dolorem pariatur velit sunt. Harum dignissimos, laborum quibusdam. Aliquam!",
-#               title: "Om teatern",
-#               image: "example.png" )
+CoOperation.create( column_1: "First Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero a, doloribus laudantium est voluptates nihil perferendis praesentium quos at officia.",
+              column_2: "Second Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quam itaque labore non illum laborum iusto quis minus rem maxime.",
+              title: "Sammarbete",
+              image: "placeholder.png" )
 
-# Course.create( column_1: "Teater (namn) är en helt nystartad teater och vänder sig till både barn och vuxna.. Pjäserna är specialskrivna för teatern och integrerar oftast publiken på olika sätt. Teaterns mål är att alltid vara lätt tillgänglig och för alla. Det är två inplanerade produktioner under 2017 en barn teater under vinter/våren och en familjeföreställning i form av vandringsteater på sommaren. Spelplatserna varierar beroende på föreställning.",
-#               column_2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id dolorem nulla earum autem, dolor rerum illo velit veniam, quam neque, recusandae fugiat voluptates optio dignissimos odit debitis iste inventore a magni quis, ipsum aliquid. Quibusdam nam modi error quasi blanditiis veniam dolorem pariatur velit sunt. Harum dignissimos, laborum quibusdam. Aliquam!",
-#               title: "Om teatern" )
+Course.create( column_1: "First Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis debitis omnis distinctio quaerat cupiditate, aspernatur unde tenetur vitae quia repellendus.",
+              column_2: "Second Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam repellat vel perspiciatis quasi animi, officiis. Natus molestias voluptates voluptate harum.",
+              title: "Kurser och utbildningar" )
+
+
+
+
+
