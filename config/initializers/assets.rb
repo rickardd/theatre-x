604526942baa47751f5f2f85cfb1fdb45516c4cd
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2016-08-05 22:38:37 +0200

    many small style fixes and file path fixes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( application_admin.css )
+
+
+config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+
