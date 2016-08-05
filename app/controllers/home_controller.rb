eef10cc563b1fba5f56fb2889d7727aba304b615
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2016-08-05 18:27:02 +0200

    Authentication system. And minor style fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
 
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
     @shows = Show.where( display: true )
     @archives = Show.where( display: false )
