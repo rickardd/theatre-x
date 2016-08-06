c1312fce5f507db59978b03cd088d41475f79ae5
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2016-08-06 10:53:09 +0200

    Heroku font test 4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,10 +10,10 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( application_admin.css )
+# Rails.application.config.assets.precompile += %w( application_admin.css )
 
 
 # config.assets.precompile += %w( .svg .eot .woff .ttf )
 # config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+# Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
