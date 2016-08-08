e552630858270f1ac81666903e7ff787e562f9dd
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2016-08-08 22:59:38 +0200

    New redirects for sing_in and sign_out actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    # request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    flash[:success] = "Signed in successfully."
+    shows_path
+  end
+
+  def after_sign_out_path_for(resource)
+    # request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    flash[:success] = "You are signed out"
+    new_user_session_path
+  end
+
+
 end
